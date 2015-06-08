f6a3e075ed26659be88ef3c668bfac9be15adcf2
Francois Pfister <francois.pfister@gmail.com>
2015-06-08 17:27:46 +0200

    correction on gmf.bridge.trace

--- org.eclipse.gmf.xpand/src/org/eclipse/gmf/internal/xpand/build/MetaModelSource.java
+++ org.eclipse.gmf.xpand/src/org/eclipse/gmf/internal/xpand/build/MetaModelSource.java
@@ -3,7 +3,6 @@ package org.eclipse.gmf.internal.xpand.build;
 import org.eclipse.emf.ecore.EPackage;
 
 public interface MetaModelSource {
-    //xyz
 	public EPackage find(String nsURI);
 	public EPackage[] all();
 }
